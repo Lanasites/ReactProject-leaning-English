f88e41a327e953cf712aba3d15d77f6277a239c4
Lanasites <mochegova@gmail.com>
2023-12-25 22:24:04 +0100

    строки в таблице

--- Project/src/Components/LineOfTable/LineOfTable.jsx
+++ Project/src/Components/LineOfTable/LineOfTable.jsx
@@ -1,13 +1,18 @@
 import './LineOfTable.scss';
+import edit_img from '../../assets/edit-green.svg';
+import delete_img from '../../assets/delete-red.svg';
 
-
-export default function TableWithAllWords(props){
+export default function LineOfTable(props){
 return(
-    <div>
-        <div>{props.slovo}</div>
-        <div>{props.perevod}</div>
-        <div>{props.trans}</div>
-        <div>{props.tema}</div>
+    <div className='LineOfTable'>
+        <div className='LineOfTable__slovo'>{props.slovo}</div>
+        <div className='LineOfTable__perevod'>{props.perevod}</div>
+        <div className='LineOfTable__trans'>{props.trans}</div>
+        <div className='LineOfTable__tema'>{props.tema}</div>
+        <div className='LineOfTable__edit'>
+            <img src={edit_img} alt="" />
+            <img src={delete_img} alt="" />
+        </div>
     </div>
 )
 
