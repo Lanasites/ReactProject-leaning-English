23f3fe5c69a53340fb2dae00f6f4db4b0cbf0b8f
Lanasites <mochegova@gmail.com>
2023-12-26 23:03:53 +0100

    добавление состояния, которое будет отвечать за показывание/скрытие перевода на карточке

--- Project/src/Components/CardOfWord/CardOfWord.jsx
+++ Project/src/Components/CardOfWord/CardOfWord.jsx
@@ -1,13 +1,21 @@
 import './CardOfWord.scss'
+import { useState } from 'react'
 
 export default function CardOfWord(){
+    const [showPerevod, setShowPerevod] = useState('disactive');
+    const [showButton, setShowButton] = useState('active');
+    const handleShowPerevod = () => {
+        setShowPerevod('active');
+        setShowButton('disactive')
+    };
     return(
         <>
         <div className="cardOfWorld">
             <div className="cardOfWorld__info">
-                <span className='cardOfWorld__slovo'>слово</span><span className='cardOfWorld__trans'>транскрипция</span>
+                <span className='cardOfWorld__slovo'>tomato</span><span className='cardOfWorld__trans'>[təˈmɑːtəʊ]</span>
             </div>
-            <div className='cardOfWorld__perevod'>Смотреть перевод</div>
+            <div className={`cardOfWorld__perevod ${showPerevod}`}>помидор</div>
+            <div className={`cardOfWorld__button ${showButton}`} onClick = {handleShowPerevod}>Смотреть перевод</div>
         </div>
         </>
     )
