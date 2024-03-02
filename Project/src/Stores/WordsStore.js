eb6fe8f138639aba5c9f79548241c46d5b1beda2
Lanasites <mochegova@gmail.com>
2024-03-03 00:01:28 +0100

    Создание store

--- Project/src/Stores/WordsStore.js
+++ Project/src/Stores/WordsStore.js
@@ -1,14 +1,50 @@
-import {action, observable} from 'mobx';
+import { action, observable, makeObservable } from 'mobx';
 
-class WordssStore  extends React.Component{
-    @observable words = []
+class WordsStore {
+    words = []
+    isLoading = false
+    error = null
 
-    @action addBook = (word) => {
-        return this.words.push(word)
+    constructor() {
+        makeObservable(this, {
+            words: observable,
+            isLoading: observable,
+            error: observable,
+            fetchWords: action,
+            updateWord: action,
+            addWord: action,
+            removeWord: action
+        })
     }
-    @action removeBook = (index) => {
-        return this.words.splice(index, 1)
+
+    fetchWords = async () => {
+        this.isLoading = true;
+
+        try {
+            const response = await fetch('http://itgirlschool.justmakeit.ru/api/words');
+            if (response.ok) {
+                const data = await response.json();
+                this.words = data;
+                this.isLoading = false;
+            } else {
+                throw new Error('Ошибка загрузки данных');
+            }
+        } catch (error) {
+            this.isLoading = false;
+            this.error = error;
+        }
+    }
+    
+    updateWord = (word) => {
+        // return this.words.push(word)
+        console.log(word)
+    }
+    addWord = (word) => {
+        this.words.push(word)
+    }
+    removeWord = (index) => {
+        this.words.splice(index, 1)
     }
 }
 
-export default WordssStore
+export default WordsStore
--- Project/src/main.jsx
+++ Project/src/main.jsx
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './Components/App'
 import '../src/style/App.scss'
 import { Provider } from 'mobx-react'
-import WordssStore from './Stores/WordsStore'
+import WordsStore from './Stores/WordsStore'
 
 const stores = {
-    wordStore: new WordssStore()
+    wordStore: new WordsStore()
 }
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(
     <Provider {...stores}>
         <App />
-    </Provider>,
-    document.getElementById('root')
-)
+    </Provider>
+);
+
